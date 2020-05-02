10efedf47b5c6d9505713f3ec7ed7d3b7bd4d927
LokirTreg <57491076+LokirTreg@users.noreply.github.com>
2020-05-02 16:49:29 +0400

    Add files via upload

--- start.py
+++ start.py
@@ -27,6 +27,13 @@ class LoginForm(FlaskForm):
     remember_me = BooleanField('Remember me')
     submit = SubmitField('Sign-up')
 
+class RegisterForm(FlaskForm):
+    email = StringField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
+    name = StringField('Имя пользователя', validators=[DataRequired()])
+
+    submit = SubmitField('Войти')
 
 @app.route('/')
 def qwe():
@@ -37,15 +44,23 @@ def qwe():
 
 @app.route('/register', methods=['GET', 'POST'])
 def qk():
+    form = RegisterForm()
     db_session.global_init("db/blogs.sqlite")
-    form = LoginForm()
+    form = RegisterForm()
     if request.method == 'POST' and form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('reg.html', title='Регистрация', form=form, message="Пароли не совпадают")
         session = db_session.create_session()
-        name = form.name.data
-        user = session.query(User).filter(User.name == name).first()
-        if user and user.check_password(form.password.data):
-            login_user(user, remember=form.remember_me.data)
-            return redirect(f"/account/{name}")
+        if session.query(User).filter(User.name == form.name.data).first():
+            return render_template('reg.html', title='Регистрация', form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data
+        )
+        user.set_password(form.password.data)
+        session.add(user)
+        session.commit()
     return render_template('reg.html', style=url_for("static", filename="css/normalize.css"),
                            style2=url_for("static", filename="css/style.css"),
                            js=url_for("static", filename="js/jQuery.js"),
@@ -53,7 +68,7 @@ def qk():
                            style3=url_for("static", filename="css/stylesu.css"),
                            form=form)
 
-app.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def qe():
     db_session.global_init("db/blogs.sqlite")
     form = LoginForm()
